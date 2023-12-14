c85ff3a61d26d448860d429ee677d49247b37163
harshana-lk <mailtoharshanaonlive@gmail.com>
2023-12-14 21:34:18 +0530

    Initial Commit

---
+++ src/test/java/me/harshu/springCRUD/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package me.harshu.springCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
